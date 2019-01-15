c820b8353923b20b83f063c6d9678cd5a8da77fa
Hannah Li <hk4li@uwaterloo.ca>
2019-01-15 15:34:21 -0500

    Upload to github, basics done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Table, TableBody, TableCell, TableRow, TextField } from '@material-ui/core';
+import data from './data.json';
 
 class App extends Component {
+  state = {
+    filter: "",
+    display: "",
+    rows: data,
+  }
+  
+
+  handleChange = event => {
+    this.setState({
+      filter: event.target.value,
+    });
+  } 
+
+  handleSearch = () => {
+    console.log(data[0].keywords);
+    this.setState({
+      display: this.state.filter,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Toronto Waste Lookup
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div style= {{margin:30}}>
+          <div className="Row">            
+            <TextField id="searchField" type="text" onChange={this.handleChange} variant="outlined" />
+            <Button id="search" variant="contained" onClick = {this.handleSearch} style={{marginLeft: 20}}>
+              HMm
+            </Button>
+          </div>
+          <p>{this.state.display}</p>
+          <Table className='Table'>
+            <TableBody>
+              {this.state.rows.map((row, index) => (
+                <TableRow className='Table_Row' key={index}>
+                  <TableCell>{row.body}</TableCell>
+                  <TableCell align="right">{row.category}</TableCell>
+                  <TableCell align="right">{row.title}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     );
   }
