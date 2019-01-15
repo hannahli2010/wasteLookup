f70aee1628a2a9b47f8d11b3dd6c1eac71037e17
Hannah Li <hk4li@uwaterloo.ca>
2019-01-15 16:24:22 -0500

    dangerously convert data.body to HTML code

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ class App extends Component {
     });
   }
 
+  rowData = 
+    <ul> <li>This item is&nbsp;<a href="https://www.toronto.ca/services-payments/recycling-organics-garbage/household-hazardous-waste/" target="_self">Household Hazardous Waste (HHW)</a>&nbsp;and MUST NOT be placed in your Garbage Bin, Blue Bin, Green Bin or put down the drain/toilet/sewer.</li> <li>Please take this item to one of the City&#x27;s&nbsp;<a href="http://www.toronto.ca/garbage/depots.htm" target="_blank" rel="noopener">Drop-Off Depots</a>&nbsp;or&nbsp;<a href="http://www.toronto.ca/environment_days/index.htm" target="_blank" rel="noopener">Community Environment Days</a>.</li> <li>For HHW between 10 to 50 litres/kilograms, residents can also request a free Toxic Taxi pick-up&nbsp;<a href="https://secure.toronto.ca/webwizard/html/household_hazardous_waste.htm">online</a>&nbsp;or by calling 311. Residents can also check the&nbsp;<a href="http://www.makethedrop.ca." target="_blank" rel="noopener">Orange Drop Program</a>.</li> </ul>   
+
+    htmlDecode(input){
+      var e = document.createElement('div');
+      e.innerHTML = input;
+      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    }
+   
+
   render() {
     return (
       <div className="App">
@@ -41,12 +51,13 @@ class App extends Component {
           </div>
           <p>{this.state.display}</p>
           <Table className='Table'>
-            <TableBody>
+            <TableBody >
               {this.state.rows.map((row, index) => (
                 <TableRow className='Table_Row' key={index}>
-                  <TableCell>{row.body}</TableCell>
-                  <TableCell align="right">{row.category}</TableCell>
-                  <TableCell align="right">{row.title}</TableCell>
+                  <TableCell>{row.title}</TableCell>
+                  <TableCell>
+                    <div dangerouslySetInnerHTML= {{__html: this.htmlDecode(row.body)}}/>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
