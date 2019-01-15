f5e8a03504a2540561a20fff4a7e365f0b35a97d
Hannah Li <hk4li@uwaterloo.ca>
2019-01-15 18:00:48 -0500

    Basic filter and favourites table added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ class App extends Component {
   state = {
     filter: "",
     display: "",
-    rows: data,
+    rows: null,
+    favourites: data.filter(item => item.keywords.search("bread bag") >= 0),
   }
   
-
   handleChange = event => {
     this.setState({
       filter: event.target.value,
+      rows: event.target.value == "" ? null : this.state.rows,
     });
   } 
 
@@ -21,18 +22,17 @@ class App extends Component {
     console.log(data[0].keywords);
     this.setState({
       display: this.state.filter,
+      rows: this.state.filter == "" 
+        ? null 
+        : data.filter(item => item.keywords.search(this.state.filter) >= 0 || item.category.search(this.state.filter) >= 0),
     });
   }
 
-  rowData = 
-    <ul> <li>This item is&nbsp;<a href="https://www.toronto.ca/services-payments/recycling-organics-garbage/household-hazardous-waste/" target="_self">Household Hazardous Waste (HHW)</a>&nbsp;and MUST NOT be placed in your Garbage Bin, Blue Bin, Green Bin or put down the drain/toilet/sewer.</li> <li>Please take this item to one of the City&#x27;s&nbsp;<a href="http://www.toronto.ca/garbage/depots.htm" target="_blank" rel="noopener">Drop-Off Depots</a>&nbsp;or&nbsp;<a href="http://www.toronto.ca/environment_days/index.htm" target="_blank" rel="noopener">Community Environment Days</a>.</li> <li>For HHW between 10 to 50 litres/kilograms, residents can also request a free Toxic Taxi pick-up&nbsp;<a href="https://secure.toronto.ca/webwizard/html/household_hazardous_waste.htm">online</a>&nbsp;or by calling 311. Residents can also check the&nbsp;<a href="http://www.makethedrop.ca." target="_blank" rel="noopener">Orange Drop Program</a>.</li> </ul>   
-
-    htmlDecode(input){
-      var e = document.createElement('div');
-      e.innerHTML = input;
-      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-    }
-   
+  htmlDecode(input){
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+  }
 
   render() {
     return (
@@ -42,26 +42,43 @@ class App extends Component {
             Toronto Waste Lookup
           </p>
         </header>
-        <div style= {{margin:30}}>
+        <div style= {{margin:30, 'flex-direction': 'column'}}>
           <div className="Row">            
             <TextField id="searchField" type="text" onChange={this.handleChange} variant="outlined" />
             <Button id="search" variant="contained" onClick = {this.handleSearch} style={{marginLeft: 20}}>
               HMm
             </Button>
           </div>
-          <p>{this.state.display}</p>
           <Table className='Table'>
             <TableBody >
-              {this.state.rows.map((row, index) => (
-                <TableRow className='Table_Row' key={index}>
-                  <TableCell>{row.title}</TableCell>
-                  <TableCell>
-                    <div dangerouslySetInnerHTML= {{__html: this.htmlDecode(row.body)}}/>
-                  </TableCell>
-                </TableRow>
-              ))}
+              {this.state.rows && 
+                this.state.rows.map((row, index) => (
+                  <TableRow className='Table_Row' key={index}>
+                    <TableCell>{row.title}</TableCell>
+                    <TableCell>
+                      <div dangerouslySetInnerHTML= {{__html: this.htmlDecode(row.body)}}/>
+                    </TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
+          <div style={{'background-color': '#f7fefa', 'align-items': 'flex-start', 'justify-content': 'flex-start','color' : '#23995c'}}>
+            <p>Favourites</p>
+              
+            <Table>
+              <TableBody >
+                {this.state.favourites && 
+                  this.state.favourites.map((row, index) => (
+                    <TableRow className='Table_Row' key={index}>
+                      <TableCell>{row.title}</TableCell>
+                      <TableCell>
+                        <div dangerouslySetInnerHTML= {{__html: this.htmlDecode(row.body)}}/>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       </div>
     );
