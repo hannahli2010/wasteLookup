b35830e8174d510935e30dae2a7ecb1df63ff92d
Hannah Li <hk4li@uwaterloo.ca>
2019-01-18 09:08:46 -0500

    changes

---
+++ precache-manifest.c8b9d31946a637e147db263cf096539c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6008e37973ed3864a0c7",
+    "url": "/wasteLookup/static/js/runtime~main.6008e379.js"
+  },
+  {
+    "revision": "c01ddfed1d4d5a63a570",
+    "url": "/wasteLookup/static/js/main.c01ddfed.chunk.js"
+  },
+  {
+    "revision": "9798bb0aa62ad4998223",
+    "url": "/wasteLookup/static/js/1.9798bb0a.chunk.js"
+  },
+  {
+    "revision": "c01ddfed1d4d5a63a570",
+    "url": "/wasteLookup/static/css/main.55c10e43.chunk.css"
+  },
+  {
+    "revision": "19c7a7cf5e92542d8d1ce70578c307f9",
+    "url": "/wasteLookup/index.html"
+  }
+];
